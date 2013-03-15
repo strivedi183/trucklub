6f852fec829683a496a4f872064d6f7d26fcdf13
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-15 19:01:17 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucklub::Application.config.secret_token = '6aa5056609f182956352515c5d036f6a652f53c0c2e0dceadf292a4bbaa824e69802399fee3a9b8f56b0c4123895af895d8b975a455a1a4d317c5d12eedb57b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Trucklub::Application.config.session_store :cookie_store, key: '_trucklub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Trucklub::Application.config.session_store :active_record_store
