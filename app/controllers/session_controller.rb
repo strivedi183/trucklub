1fa992c9bdd112ea2c0c2a8bc5851018db9c293f
Ron Jones <rrrron@gmail.com>
2013-03-16 14:49:58 -0400

    added redirects to session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,8 +6,10 @@ class SessionController < ApplicationController
     @auth = User.where(:email => params[:email]).first
     if @auth.present? && @auth.authenticate(params[:password])
       session[:user_id] = @auth.id
+      redirect_to root_path
     else
       session[:user_id] = nil
+      redirect_to login_path
     end
   end
 
