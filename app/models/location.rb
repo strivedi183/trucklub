2d759f558aa23ae1235bf4e21e15eaa3a9226f31
Tony Contreras <tony.contreras@gmail.com>
2013-03-16 12:18:24 -0400

    edited seed file and location model relationship

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,26 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id             :integer          not null, primary key
+#  location_date  :date
+#  arrival_time   :time
+#  departure_time :time
+#  address        :string(255)
+#  latitude       :float
+#  longitude      :float
+#  truck_id       :integer
+#  secret_code    :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'random-word'
 class Location < ActiveRecord::Base
   attr_accessible :location_date, :arrival_time, :departure_time, :address
   belongs_to :truck
   before_save :geocode, :generate_secret
+  validates :truck_id, :presence => true
 
   private
   def geocode
