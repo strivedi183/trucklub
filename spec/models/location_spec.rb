fad76e777a3c3a04ce5e6a2b3d7ca68d32d3fcb1
Tony Contreras <tony.contreras@gmail.com>
2013-03-16 19:03:37 -0400

    changed button orders on forms, and made secret words no more than 7 chars

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -30,9 +30,10 @@ describe 'Location' do
       expect(location.longitude).to eq(-73.9855692)
     end
 
-    it 'generates a secret word' do
+    it 'generates a secret word no longer than 7 characters' do
       location = Location.create(location_date: '2013-01-20', arrival_time: '10:00', departure_time: '14:00', address: 'Empire State Building, NYC' )
       expect(location.secret_code).to_not be_nil
+      expect(location.secret_code.length).to be < 7
     end
 
   end
