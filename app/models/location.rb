af4b39f325e7dcb9ed9b9326c20c12a000a524b7
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-16 17:18:17 -0400

    Changed truck models and seed file in order to fix relationship with locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -18,10 +18,14 @@
 require 'random-word'
 class Location < ActiveRecord::Base
   attr_accessible :location_date, :arrival_time, :departure_time, :address
-  belongs_to :truck
+  belongs_to :truck, :inverse_of => :locations
   before_save :geocode, :generate_secret
   #validates :truck_id, :presence => true
 
+  def send_code_text
+    client = Twilio::REST::Client.new(ENV['TW_SID'], ENV['TW_TOK'])
+    client.account.sms.messages.create(:from => '+17172301041', :to => self.truck.phone, :body => self.secret_code)
+  end
 
   private
   def geocode
