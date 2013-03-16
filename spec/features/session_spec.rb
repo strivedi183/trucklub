259ab819d13c1d3a46596b198b8f7254caa52489
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-16 14:49:18 -0400

    Started login tests in session_spec.rb

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -16,4 +16,23 @@ describe 'Session' do
       page.should have_button('Get Truckin')
     end
   end
+
+  describe 'POST /login' do
+    let(:user) {User.create(name: 'bob', email: 'bob@gmail.com', password: 'a', password_confirmation: 'a')}
+
+    it 'logs the user into the system if credentials are correct' do
+      visit root_path
+      click_link('Login to trucKlub')
+      fill_in('Email', :with => user.email)
+      fill_in('Password', :with => 'a')
+      click_button('Get Truckin')
+      page.should_not have_button('Get Truckin')
+      # expect(page.has_link?('bob')).to be true
+      # page.should_not have_link('Register')
+      # page.should_not have_link('Login')
+      # visit root_path
+      # page.should_not have_link('Login to trucKlub')
+      # page.should_not have_link('Register')
+    end
+  end
 end
