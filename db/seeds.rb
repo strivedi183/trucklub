01ea9cd09f7d0b506c6de259a46100a0c21a4c09
Tony Contreras <tony.contreras@gmail.com>
2013-03-16 13:31:38 -0400

    added truck views controllers and first test, and modified seed fil

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ Location.delete_all
 
 
 t1 = Truck.create(:title => 'Fishing Shrimp', :description => 'Seafood', :twitter => 'FishingShrimp' , :phone => '914-649-8398')
-t2 = Truck.create(:title => 'Lukes Lobsters', :description => 'Seafood', :twitter => 'LukesLobsterNY' , :phone => '')
-t3 = Truck.create(:title => 'Mud Truck', :description => 'Coffee', :twitter => 'mudpeople' , :phone => '')
-t4 = Truck.create(:title => 'Crusin Kitchen', :description => 'American', :twitter => 'Cruisin_Kitchen' , :phone => '')
+t2 = Truck.create(:title => 'Lukes Lobsters', :description => 'Seafood', :twitter => 'LukesLobsterNY' , :phone => '917-922-2816')
+t3 = Truck.create(:title => 'Mud Truck', :description => 'Coffee', :twitter => 'mudpeople' , :phone => '212-555-1221')
+t4 = Truck.create(:title => 'Crusin Kitchen', :description => 'American', :twitter => 'Cruisin_Kitchen' , :phone => '212-555-1212')
 
 l1 = Location.create(location_date: '2013-03-16', arrival_time: '10:00', departure_time: '14:00', address: 'Empire State Building, NYC' )
 l2 = Location.create(location_date: '2013-03-16', arrival_time: '10:00', departure_time: '14:00', address: 'Union Square, NYC' )
 l3 = Location.create(location_date: '2013-03-16', arrival_time: '10:00', departure_time: '14:00', address: '920 Broadway, NYC' )
 
-l1.truck_id = t1.id
-l1.truck_id = t1.id
-l3.truck_id = t3.id
+l1.truck = t1
+l2.truck = t2
+l3.truck = t3
