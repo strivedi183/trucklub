3e9d418b7287fbac67a3def0810a44b0eab47618
Tony Contreras <tony.contreras@gmail.com>
2013-03-15 21:00:40 -0400

    holy shit, geocoder test worked on first try

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id             :integer          not null, primary key
+#  location_date  :date
+#  arrival_time   :time
+#  departure_time :time
+#  address        :string(255)
+#  latitude       :float
+#  longitude      :float
+#  secret_code    :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'spec_helper'
 
 describe 'Location' do
@@ -5,6 +21,12 @@ describe 'Location' do
     it 'creates a new instance of Location' do
       location = Location.create(location_date: '2013-01-20', arrival_time: '10:00', departure_time: '14:00', address: 'Empire State Building, NYC' )
       expect(location).to be_an_instance_of(Location)
+    end
+
+    it 'grabs a latitude and longitude from a new instance' do
+      location = Location.create(location_date: '2013-01-20', arrival_time: '10:00', departure_time: '14:00', address: 'Empire State Building, NYC' )
+      expect(location.latitude).to eq(40.7482845)
+      expect(location.longitude).to eq(-73.9855692)
     end
   end
 end
