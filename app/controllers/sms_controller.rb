3f057be72b325348ba400d525a72a2960c5f2449
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-16 20:11:12 -0400

    Removed binding.pry from sms_controller.rb

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,6 +1,5 @@
 class SmsController < ApplicationController
   def receive
-    binding.pry
     message_body = params["Body"]
     from_number = params["From"]
     truck = Truck.where(:phone => from_number)
