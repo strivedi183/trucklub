56acf43eb35915d289990471694c7fac89a1d25e
Tony Contreras <tony.contreras@gmail.com>
2013-03-17 00:50:43 -0400

    form test

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,8 +1,8 @@
 class SmsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def receive
-    message_body = params["Body"]
-    from_number = params["From"]
+    message_body = params["body"]
+    from_number = params["from"]
     truck = Truck.where(:phone => from_number).first
     location = truck.locations.last
     location.address = message_body
