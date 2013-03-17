ca47fb2df39caea40101add6bd453cac598dfe94
Tony Contreras <tony.contreras@gmail.com>
2013-03-17 02:06:03 -0400

    this is getting ridiculous

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -6,7 +6,7 @@ class SmsController < ApplicationController
     #t1 = Truck.where(:phone => from_number).first
     t1 = Truck.find(15)
     l1 = t1.locations.last
-    l1.address = message_body.to_s
+    l1.address = message_body
     l1.save
     render :nothing => true
   end
