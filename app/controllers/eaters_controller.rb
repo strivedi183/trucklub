ed32394c50465f2703b9cec8206669be257c094a
Tony Contreras <tony.contreras@gmail.com>
2013-03-17 17:20:22 -0400

    fill card coming in with ajax

--- app/controllers/eaters_controller.rb
+++ app/controllers/eaters_controller.rb
@@ -1,12 +1,12 @@
 class EatersController < ApplicationController
   def create_code
-      binding.pry
-
     user_code = params[:code]
     todays_locations = Location.where("created_at >= ?", Time.zone.now.beginning_of_day)
     secret_codes = todays_locations.map(&:secret_code)
     if user_code.in?secret_codes
+      Meal.create()
       render :create_code
+
     else
       render :error
     end
