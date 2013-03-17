112a0a0a92086e1a181ce050b1c21424a9e308a3
Tony Contreras <tony.contreras@gmail.com>
2013-03-17 02:00:47 -0400

    temp hard coding truckid

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,8 +2,9 @@ class SmsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def receive
     message_body = params["body"]
-    from_number = params["from"]
-    t1 = Truck.where(:phone => from_number).first
+    #from_number = params["from"]
+    #t1 = Truck.where(:phone => from_number).first
+    t1 = Truck.find(15)
     l1 = t1.locations.last
     l1.address = message_body.to_s
     l1.save
