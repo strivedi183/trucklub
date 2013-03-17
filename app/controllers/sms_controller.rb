446dacdfc5ceac95ddb77d0f9caf12cf2f703659
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-16 20:23:59 -0400

    Changes to sms_controller.rb to test

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,8 +3,11 @@ class SmsController < ApplicationController
   def receive
     message_body = params["Body"]
     from_number = params["From"]
-    truck = Truck.where(:phone => from_number)
-    truck.title = params["Body"]
-    truck.save
+    puts params
+    puts params[:from]
+    puts from_number
+    # truck = Truck.where(:phone => from_number)
+    # truck.title = params["Body"]
+    # truck.save
   end
 end
